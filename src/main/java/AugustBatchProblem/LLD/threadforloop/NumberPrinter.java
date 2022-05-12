4ebe2cee6480991d593d0b030a00db0ade862ec2
jayeshdalal <jayesh.dalal@go-jek.com>
2022-05-12 17:55:36 +0530

    [jayesh] - adding thread problems.

---
+++ src/main/java/AugustBatchProblem/LLD/threadforloop/NumberPrinter.java
@@ -0,0 +1,17 @@
+package AugustBatchProblem.LLD.threadforloop;
+
+public class NumberPrinter implements Runnable {
+    private  int number;
+
+    NumberPrinter(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        if (this.number == 50) {
+            System.out.println("Wait");
+        }
+        System.out.println("The number is " + number);
+    }
+}
