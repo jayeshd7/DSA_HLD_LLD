d80b96bb987144a02719be6c29dd3608be10e567
jdalal2 <jdalal2@slb.com>
2025-01-08 21:52:42 +0530

    [Jayesh] - Week-3 day -3  GridUniquePath.java

---
+++ src/main/java/sdeSheet/LargestSubarraywithKsum.java
@@ -0,0 +1,16 @@
+package sdeSheet;
+
+public class LargestSubarraywithKsum {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 1, 1, 2, 3, 5};
+        int k = 5;
+        int ans = largestSubarray(arr, k);
+        System.out.println("Largest subarray with sum k: " + ans);
+    }
+
+    private static int largestSubarray(int[] arr, int k) {
+
+        return 1;
+    }
+}
